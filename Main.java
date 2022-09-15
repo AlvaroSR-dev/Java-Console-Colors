4c326cf09105c4013784cdf6103cd3ffab8b0e5f
Álvaro Sánchez <73520829+AlvaroSR-dev@users.noreply.github.com>
2022-09-15 18:12:11 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ConsoleColors.RED_BACKGROUND + "RED COLORED" + ConsoleColors.BLUE + " NORMAL");
+    }
+}
